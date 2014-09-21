a5a6380058686462c843d8cb8463ba9c3672fdc1
Alexey Melnikov <individum05@gmail.com>
2014-09-21 23:38:02 +0700

    lab01/task3 fixes

--- lab01/task3/task3.cpp
+++ lab01/task3/task3.cpp
@@ -1,45 +1,55 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-vector <int> get_linepos(fstream &input)
+ios::pos_type get_filesize(fstream &file)
 {
-	vector <int> pos_vec;
-	int position = 0;
-	string line;
+	ios::pos_type size;
 
-	while (getline(input, line)) {
-		position += line.length();
-		if (!input.eof())
-			++position;
-		pos_vec.push_back(position);
+	file.seekg(0, ios_base::end);
+	size = file.tellg();
+	file.clear();
+	return size;
+}
+
+void get_linepos(fstream &inout)
+{
+	ios::pos_type size;
+	int cnt = 0;
+	char ch;
+
+	size = get_filesize(inout);
+	inout << endl;
+	inout.seekg(0);
+	while (inout.get(ch) && (cnt <= size)) {
+		cnt++;
+		if (ch == '\n') {
+			ios::pos_type position = inout.tellg();
+			inout << cnt << ' ';
+			inout.seekg(position);
+		}
 	}
-	input.clear();
-	return pos_vec;
+	inout.clear();
+	inout << cnt << endl;
 }
 
 int main(int argc, char *argv[])
 {
 	fstream posfile;
-	vector <int> pos_vec;
+	long filesize;
 
 	if (argc != 2) {
 		cerr << "Uncorrect command line arguments!\n";
 		return -1;
 	}
-	posfile.open(argv[1]);
+	posfile.open(argv[1], ios_base::in | ios_base::out | ios_base::app);
 	if (!posfile.is_open()) {
 		cerr << "Cannot open file: " << argv[1] << endl;
 		return -2;
 	}
-	pos_vec = get_linepos(posfile);
-	posfile << '\n';
-	for (int i = 0; i < pos_vec.size(); i++) {
-		posfile << pos_vec[i] << ' ';
-	}
+	get_linepos(posfile);
 	posfile.close();
 	return 0;
 }

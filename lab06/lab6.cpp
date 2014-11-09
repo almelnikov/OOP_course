ccf98d9435dcbbae080b8605f413950ad1308a6a
Alexey Melnikov <individum05@gmail.com>
2014-11-10 00:13:43 +0600

    Displace split cycle with accumulate

--- lab06/lab6.cpp
+++ lab06/lab6.cpp
@@ -18,27 +18,32 @@ bool is_letter(char c)
 		return false;
 }
 
-vector <string> &split_string(vector <string> &strvec, string &str)
+vector <string> &split_step(vector <string> &acc, char c)
 {
-	string::iterator it;
-	string::iterator letter_change;
-	bool flag_letter;
-	string buf;
-	
-	if (!str.empty())
-		flag_letter = is_letter(str[0]);
-	for (it = str.begin(); it != str.end(); ++it) {
-		if (is_letter(*it) != flag_letter) {
-			strvec.push_back(buf);
-			buf.clear();
-			buf.push_back(*it);
-			flag_letter = !flag_letter;
-		}
-		else
-			buf.push_back(*it);
+	if (acc.empty()) {
+		string str;
+		str.push_back(c);
+		acc.push_back(str);
 	}
-	strvec.push_back(buf);
-	return strvec;
+	else {
+		size_t last = acc.size() - 1;
+		if (acc[last].empty()) {
+			string str;
+			str.push_back(c);
+			acc.push_back(str);
+		}
+		else {
+			char last_char = acc[last][acc[last].size() - 1];
+			if (is_letter(last_char) == is_letter(c))
+				acc[last].push_back(c);
+			else {
+				string str;
+				str.push_back(c);
+				acc.push_back(str);
+			}
+		}
+	}
+	return acc;
 }
 
 string &find_and_replace_str(map <string, string> wordsmap, string &str)
@@ -124,7 +129,13 @@ vector <pair <string, string> > split_on_pairs(vector <string> &vec)
 {
 	vector <pair <string, string> > res;
 
-	return accumulate(vec.begin(), vec.end(), res, add_pair);
+	res = accumulate(vec.begin(), vec.end(), res, add_pair);
+	if (!res.empty())
+		if (res[res.size() - 1].second.empty()) {
+			cerr << "Odd number of imput words" << endl;
+			res.pop_back();
+		}
+	return res;
 }
 
 int main(int argc, char *argv[])
@@ -151,21 +162,19 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 	
+	// Получаем токены из файла
 	vector <string> keyv_words = file_tokenizer(keyvalue);
-	for (size_t i = 0; i < keyv_words.size(); i++)
-		cout << keyv_words[i] << ' ';
+	// Разбиваем их на пары
 	vector <pair <string, string> > kvpairs = split_on_pairs(keyv_words);
 	for_each(kvpairs.begin(), kvpairs.end(), AddToMap(&wordsmap));
-	/*
-	while (keyvalue >> skey >> svalue) {
-		wordsmap.insert(pair <string, string> (skey, svalue));
-	}
-	*/
-	while (getline(transformed, line)) {
-		if (!transformed.eof())
-			line.append("\n");
-		split_string(file_content, line);
-	}
+	
+	// Считываем файл целиком и делим его на токены
+	stringstream strs;
+		strs << transformed.rdbuf();
+	string filestr = strs.str();
+	file_content = accumulate(filestr.begin(), filestr.end(),
+			   file_content, split_step);;
+
 	transformed.close();
 	transformed.open(argv[2], ios_base::out | ios_base::trunc);
 
